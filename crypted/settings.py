ae0de3a3aacd35d4e66734a25fe50611b17279a3
mmby85 <77107016+mmby85@users.noreply.github.com>
2021-09-13 09:20:11 +0100

    Update settings.py

--- crypted/settings.py
+++ crypted/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ir3p1!)t-g+0so$@5(apj@mx_-cn9zr2c2e4rw8fm*t36llj&l'
+SECRET_KEY = 'changeme'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
