ad6bc579e35c2bec362baad49de7c1388007a9ec
mongi <mmby85@yahoo.fr>
2021-09-18 23:44:12 +0100

    last2

--- UsersAuth/models.py
+++ UsersAuth/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+import datetime
 
 folder = 'static/documents/'
 
@@ -26,4 +27,7 @@ class Message(models.Model):
     sentfrom = models.CharField(max_length=255, default=None,blank=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
     password = models.CharField(max_length=2000)
+
+    def __str__(self) -> str:
+        return str(self.uploaded_at.strftime("%d-%m-%y %H:%M:%S")) + " | " + str(self.sentfrom) + "->" + str(self.sento.username)  + " | " + str(self.document.name)
     
